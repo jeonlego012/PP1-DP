f13ef271673730bf1cef0e657c112f34fae65585
jeonlego012 <21700643@handong.edu>
2018-10-23 21:49:29 +0900

    Task3

--- src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
+++ src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
@@ -60,9 +60,16 @@ public class CLAMITest {
 		// TASK3: Conduct defect prediction by using CLAMI models
 		// complete this test case for the generated arff files from Task 2
 		
-		//String[] args = {"-f","data/[your_generated_metric_data].arff","-l","label","-p", "buggy", "-m"};
+		String[] args = {"-f","data/PP1test.arff","-l","label","-p", "buggy", "-m"};
 		
-		//CLAMI.main(args);
+		CLAMI.main(args);
+	}
+	@Test
+	public void testCLAMIWithYourSelectedProject2() {
+		
+		String[] args = {"-f","data/PP1test2.arff","-l","label","-p", "buggy", "-m"};
+		
+		CLAMI.main(args);
 	}
 	
 	@Test
