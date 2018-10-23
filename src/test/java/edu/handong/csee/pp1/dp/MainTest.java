4c3ae2adaceb61d9ad9e4e7a2e7f3678a363774f
jeonlego012 <21700643@handong.edu>
2018-10-23 21:37:35 +0900

    Task2

--- src/test/java/edu/handong/csee/pp1/dp/MainTest.java
+++ src/test/java/edu/handong/csee/pp1/dp/MainTest.java
@@ -26,6 +26,14 @@ public class MainTest {
 		// TASK2 TODO GitHub에서 작은 규모의 자바 프로젝트를 2개를 찾아, arff파일을 생성하시오. 파일이름은 [project이름].arff data디렉터리에 저장되게 하고
 		// 생성이 된 후 commit/push해야 함.
 		// 위 test main을 참고하여 실행하면 됨.
-		
+		String[] args = {"/Users/youngone/PP1test/Java", "data/PP1test.arff"};
+		assertEquals(args.length, 2);
+		Main.main(args);
+	}
+	@Test
+	public void testForTask22() {
+		String[] args = {"/Users/youngone/ppbd3/java", "data/PP1test2.arff"};
+		assertEquals(args.length, 2);
+		Main.main(args);
 	}
 }
